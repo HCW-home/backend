f9ab95d4a1e2607e2f779e7386e5cb349dfe8364
Muhammed <aapozaid@gmail.com>
2022-03-03 18:59:00 +0200

    exit app if clamscan didn't connect

--- api/hooks/initClamscn.js
+++ api/hooks/initClamscn.js
@@ -1,27 +1,27 @@
-const NodeClam = require('clamscan');
-module.exports = function myBasicHook (sails) {
+const NodeClam = require("clamscan");
+module.exports = function myBasicHook(sails) {
   return {
-    async initialize (cb) {
-
+    async initialize(cb) {
       try {
         const clamscan = await new NodeClam().init({
           remove_infected: true,
           clamdscan: {
-            socket: process.env.CLAM_SOCKET || '/var/run/clamd.scan/clamd.sock' // Socket file for connecting via TCP
+            socket: process.env.CLAM_SOCKET || "/var/run/clamd.scan/clamd.sock", // Socket file for connecting via TCP
           },
-          preference: 'clamdscan'
+          preference: "clamdscan",
         });
 
         sails.config.globals.clamscan = clamscan;
         // Do some stuff here to initialize hook
         // And then call `cb` to continue
         sails.config.startCron();
-
       } catch (error) {
-        console.error("Error initializing clamscan: " , error);
+        console.error("Error initializing clamscan: ", error);
+        if (process.env.NODE_ENV === "production") {
+          process.exit(1);
+        }
       }
       return cb();
-
-    }
+    },
   };
 };
