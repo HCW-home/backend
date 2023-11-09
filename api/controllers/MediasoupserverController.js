89ee222f8378287b21d03385281da47b978a45e0
Gor Grigoryan <gorgrigoryan1997@gmail.com>
2023-11-09 14:40:24 +0400

    validator for checking output

--- api/controllers/MediasoupserverController.js
+++ api/controllers/MediasoupserverController.js
@@ -2,19 +2,7 @@ const validator = require('validator');
 module.exports = {
   create: async function (req, res) {
     try {
-      const url = validator.escape(req.body.url).trim();
-      const username = validator.escape(req.body.username).trim();
-      const password = validator.escape(req.body.password).trim();
-      const maxNumberOfSessions = validator.escape(req.body.maxNumberOfSessions).trim();
-      const active = validator.escape(req.body.active).trim();
-      const  data = {
-        url,
-        username,
-        password,
-        maxNumberOfSessions,
-        active
-      }
-
+      let data = req.body;
       let server = await sails.models.mediasoupserver.create(data).fetch();
       return res.ok(server);
     } catch (error) {
@@ -33,8 +21,19 @@ module.exports = {
 
   update: async function (req, res) {
     try {
-      let data = req.body;
-      let serverId = req.params.id;
+      const url = validator.escape(req.body.url).trim();
+      const username = validator.escape(req.body.username).trim();
+      const password = validator.escape(req.body.password).trim();
+      const maxNumberOfSessions = validator.escape(req.body.maxNumberOfSessions).trim();
+      const active = validator.escape(req.body.active).trim();
+      const  data = {
+        url,
+        username,
+        password,
+        maxNumberOfSessions,
+        active
+      }
+      const serverId = validator.escape(req.params.id).trim();
       let server = await sails.models.mediasoupserver.updateOne({ id: serverId }).set(data);
       return res.ok(server);
     } catch (error) {
