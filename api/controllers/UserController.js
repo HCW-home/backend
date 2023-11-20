8049c785466d08027d8511e9e2c8b70642195d2d
Gilles Hemmerlé <giloux@gmail.com>
2023-11-20 16:31:49 +0100

    Fixed dot removed from gmail addresses

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -4,71 +4,77 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
-const validator = require('validator');
+const validator = require("validator");
 
 module.exports = {
-
-
-  ip (req, res) {
-
-
+  ip(req, res) {
     res.json({ ip: req.ip });
   },
 
-  async addDoctorToQueue (req, res) {
-
+  async addDoctorToQueue(req, res) {
     if (!req.body.queue) {
-      return res.status(400).json({ message: 'queue is required' });
+      return res.status(400).json({ message: "queue is required" });
     }
 
-    await User.addToCollection(req.params.user, 'allowedQueues', req.body.queue);
+    await User.addToCollection(
+      req.params.user,
+      "allowedQueues",
+      req.body.queue
+    );
 
     return res.status(200).json({ success: true });
   },
 
-  async removeDoctorFromQueue (req, res) {
-
+  async removeDoctorFromQueue(req, res) {
     if (!req.body.queue) {
-      return res.status(400).json({ message: 'queue is required' });
+      return res.status(400).json({ message: "queue is required" });
     }
 
     try {
-      const userAndQueueExist = await User.findOne({ id: req.params.user }).populate('allowedQueues', { id: req.body.queue });
+      const userAndQueueExist = await User.findOne({
+        id: req.params.user,
+      }).populate("allowedQueues", { id: req.body.queue });
 
       if (!userAndQueueExist) {
         res.status(404);
-        return res.json({ message: 'User not found' });
-      }
-      else if (userAndQueueExist.allowedQueues.length === 0) {
+        return res.json({ message: "User not found" });
+      } else if (userAndQueueExist.allowedQueues.length === 0) {
         res.status(404);
-        return res.json({ message: 'Queue not found' });
+        return res.json({ message: "Queue not found" });
       }
 
-      await User.removeFromCollection(req.params.user, 'allowedQueues', req.body.queue);
+      await User.removeFromCollection(
+        req.params.user,
+        "allowedQueues",
+        req.body.queue
+      );
 
       return res.status(200).json({ success: true });
-
     } catch (err) {
       return res.badRequest(err);
     }
   },
 
-  async getDoctorQueues (req, res) {
-
-
-    const user = await User.findOne({ id: req.params.user }).populate('allowedQueues');
+  async getDoctorQueues(req, res) {
+    const user = await User.findOne({ id: req.params.user }).populate(
+      "allowedQueues"
+    );
 
     return res.status(200).json(user.allowedQueues);
   },
 
-  async getUser (req, res) {
+  async getUser(req, res) {
     const user = await User.findOne({ id: req.params.user });
     return res.status(200).json(user);
   },
 
-  registerNurse: async function(req, res) {
+  registerNurse: async function (req, res) {
     try {
-      const email = validator.normalizeEmail(req.body.email);
+      const email = validator.normalizeEmail(req.body.email, {
+        gmail_remove_dots: false,
+      });
+      // const email = validator.normalizeEmail(req.body.email);
+
       const firstName = validator.escape(req.body.firstName).trim();
       const lastName = validator.escape(req.body.lastName).trim();
       const phoneNumber = validator.escape(req.body.phoneNumber).trim();
@@ -76,9 +82,8 @@ module.exports = {
       const country = validator.escape(req.body.country).trim();
       const sex = validator.escape(req.body.sex).trim();
 
-
       if (!validator.isEmail(email)) {
-        return res.badRequest({ error: 'Invalid email address.' });
+        return res.badRequest({ error: "Invalid email address." });
       }
 
       const existingUser = await User.findOne({
@@ -86,7 +91,7 @@ module.exports = {
         role: sails.config.globals.ROLE_NURSE,
       });
       if (existingUser) {
-        return res.badRequest({ error: 'Email already in use.' });
+        return res.badRequest({ error: "Email already in use." });
       }
 
       const newUser = await User.create({
@@ -98,18 +103,16 @@ module.exports = {
         country,
         sex,
         role: sails.config.globals.ROLE_NURSE,
-        status: 'not-approved'
+        status: "not-approved",
       }).fetch();
 
       return res.ok(newUser);
-
     } catch (error) {
       return res.serverError(error);
     }
   },
 
-
-  async updateNotif (req, res) {
+  async updateNotif(req, res) {
     const valuesToUpdate = {};
     if (req.body.enableNotif !== undefined) {
       valuesToUpdate.enableNotif = req.body.enableNotif;
@@ -121,28 +124,29 @@ module.exports = {
     return res.status(200).json({ success: true });
   },
 
-
-  updateStatus: async function(req, res) {
+  updateStatus: async function (req, res) {
     try {
-      const userId = validator.escape(req.param('id')).trim();
+      const userId = validator.escape(req.param("id")).trim();
       const newStatus = req.body.status;
 
-      if (!['approved', 'not-approved'].includes(newStatus)) {
-        return res.badRequest({ error: 'Invalid status value.' });
+      if (!["approved", "not-approved"].includes(newStatus)) {
+        return res.badRequest({ error: "Invalid status value." });
       }
 
       const user = await User.findOne({ id: userId });
       if (!user) {
-        return res.notFound({ error: 'User not found.' });
+        return res.notFound({ error: "User not found." });
       }
 
-      const updatedUser = await User.updateOne({ id: userId }).set({ status: newStatus });
+      const updatedUser = await User.updateOne({ id: userId }).set({
+        status: newStatus,
+      });
 
       return res.ok(updatedUser);
     } catch (error) {
       return res.serverError(error);
     }
-  }
+  },
 
   // async count(req, res){
   //   let count
@@ -163,6 +167,4 @@ module.exports = {
   //     count
   //   })
   // }
-
 };
-
