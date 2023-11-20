8c47fe3c2ad130633de5997aee3eda81f82b7bd0
Gilles Hemmerlé <giloux@gmail.com>
2023-11-20 15:24:30 +0100

    P021417-04 - Updated the policies for isConsultationOwner

--- api/policies/isConsultationOwner.js
+++ api/policies/isConsultationOwner.js
@@ -1,55 +1,80 @@
-const parseConsultationId = require('./utils/parseConsultationId')
+const parseConsultationId = require("./utils/parseConsultationId");
 module.exports = async function (req, res, proceed) {
+  const consultationId = parseConsultationId(req, res);
 
-  const consultationId =  parseConsultationId(req, res)
-
-  if(!consultationId){
-    return res.notFound()
+  if (!consultationId) {
+    return res.notFound();
   }
   let consultation;
-  const {role} = req.user
-  if (role === 'nurse' || role === 'patient') {
-
+  const { role } = req.user;
+  if (role === "nurse" || role === "patient") {
     consultation = await Consultation.count({
       id: consultationId,
-      owner: req.user.id
+      owner: req.user.id,
     });
-
-  } else if (role === 'doctor' || role === 'admin') {
-
+  } else if (role === "doctor" || role === "admin") {
     consultation = await Consultation.count({
       id: consultationId,
       or: [
         { acceptedBy: req.user.id, _id: consultationId },
-        { acceptedBy: null }
-      ]
+        { acceptedBy: null },
+      ],
     });
 
-  }else if(role === 'scheduler'){
+    // Check if the user does have access to this specific consultation
+    consultation = null;
+    // If the authenticated account accepted it, allow access...
     consultation = await Consultation.count({
       id: consultationId,
-      invitedBy: req.user.id
+      acceptedBy: req.user.id,
     });
-  }else if(role === 'translator'){
+
+    if (!consultation) {
+      // If accepted by someone else, not allow read it
+      consultation = await Consultation.count({
+        id: consultationId,
+        and: [
+          { acceptedBy: { "!=": null } },
+          { acceptedBy: { "!=": req.user.id } },
+        ],
+      });
+    }
+
+    // Check if the consultation is in an accessible queue and not accepted yet
+    if (!consultation) {
+      const myQueues = req.user.allowedQueues;
+      if (myQueues) {
+        consultation = await Consultation.count({
+          id: consultationId,
+          acceptedBy: null,
+          queue: { in: myQueues.map((q) => q.id) },
+        });
+      }
+    }
+  } else if (role === "scheduler") {
     consultation = await Consultation.count({
       id: consultationId,
-      translator: req.user.id
+      invitedBy: req.user.id,
     });
-  }else if (role === 'guest'){
+  } else if (role === "translator") {
     consultation = await Consultation.count({
       id: consultationId,
-      guest: req.user.id
+      translator: req.user.id,
     });
-  } else if (role === 'expert'){
+  } else if (role === "guest") {
     consultation = await Consultation.count({
       id: consultationId,
-      experts: req.user.id
+      guest: req.user.id,
     });
-  }else{
-    return res.notFound()
+  } else if (role === "expert") {
+    consultation = await Consultation.count({
+      id: consultationId,
+      experts: req.user.id,
+    });
+  } else {
+    return res.notFound();
   }
   if (!consultation) {
-
     return res.forbidden();
   }
 
