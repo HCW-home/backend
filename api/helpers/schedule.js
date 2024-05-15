93d636a88f86780732f052fbdb0f21996b2eaf43
Gor Grigoryan <gorgrigoryan1997@gmail.com>
2024-05-15 12:28:45 +0400

    Invalid helper name log

--- api/helpers/schedule.js
+++ api/helpers/schedule.js
@@ -2,37 +2,37 @@ const { CronJob } = require('cron');
 const { inviteJobs } = require('../../config/cron');
 
 module.exports = {
-  friendlyName: "Schedule",
+  friendlyName: 'Schedule',
 
-  description: "Schedule something.",
+  description: 'Schedule something.',
 
   inputs: {
     name: {
-      type: "string",
+      type: 'string',
       required: true,
     },
     time: {
-      type: "ref",
+      type: 'ref',
       required: true,
     },
     data: {
-      type: "ref",
+      type: 'ref',
     },
   },
 
   exits: {
     success: {
-      description: "All done.",
+      description: 'All done.',
     },
     invalidHelper: {
-      description: "Invalid helper name provided.",
+      description: 'Invalid helper name provided.',
     },
     error: {
-      description: "An error occurred.",
+      description: 'An error occurred.',
     },
   },
 
-  fn: async function (inputs, exits) {
+  fn: async function(inputs, exits) {
     try {
       // if (!inviteJobs[inputs.name]) {
       //   sails.log.error(`Invalid helper name: ${inputs.name}`);
@@ -43,20 +43,28 @@ module.exports = {
         const now = new Date();
         const delay = inputs.time.getTime() - now.getTime();
         if (delay > 0) {
-          sails.log.info(`Scheduling job '${inputs.name}' to run in ${delay} ms`);
-          setTimeout(async () => {
-            await inviteJobs[inputs.name](inputs.data.invite);
-          }, delay);
+          if (inviteJobs[inputs.name]) {
+            sails.log.info(`Scheduling job '${inputs.name}' to run in ${delay} ms`);
+            setTimeout(async () => {
+              await inviteJobs[inputs.name](inputs.data.invite);
+            }, delay);
+          } else {
+            sails.log.info(`Invalid helper name: '${inputs.name}'`);
+          }
         } else {
           sails.log.error(`Invalid time: ${inputs.time}`);
           return exits.error();
         }
       } else {
         sails.log.info(`Scheduling job '${inputs.name}' with cron string '${inputs.time}'`);
-        const job = new CronJob(inputs.time, async () => {
-          await inviteJobs[inputs.name](inputs.data.invite);
-        }, null, true);
+        if (inviteJobs[inputs.name]) {
+          const job = new CronJob(inputs.time, async () => {
+            await inviteJobs[inputs.name](inputs.data.invite);
+          }, null, true);
         job.start();
+        } else {
+          sails.log.info(`Invalid helper name: '${inputs.name}'`);
+        }
       }
 
       return exits.success();
