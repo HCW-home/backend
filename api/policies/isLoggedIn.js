c5209b20e1ebda786e03006ed3bb7890363484e6
Gor Grigoryan <gorgrigoryan1997@gmail.com>
2024-09-18 16:08:38 +0400

    refs #6640, Don't show message in backend when jwt expired

--- api/policies/isLoggedIn.js
+++ api/policies/isLoggedIn.js
@@ -6,7 +6,6 @@ module.exports = function (req, res, proceed) {
   if (!req.headers['x-access-token'] && !req.query.token) { return res.status(401).json({ error: 'Unauthorized' }); }
   jwt.verify(req.headers['x-access-token'] || req.query.token, sails.config.globals.APP_SECRET, async (err, decoded) => {
     if (err) {
-      console.error('error ', err);
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
