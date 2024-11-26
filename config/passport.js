507b9bf40fc4cf8c5b00cac71b40223afa99ea10
Gor Grigoryan <gorgrigoryan1997@gmail.com>
2024-11-26 17:23:28 +0100

    fix crash on finding conflicting Users

--- config/passport.js
+++ config/passport.js
@@ -455,12 +455,12 @@ if (process.env.LOGIN_METHOD === 'openid') {
         }
         let user
 
-        let conflictingUser = await User.findOne({
-          email,
+        let conflictingUsers = await User.find({
+          email: profile[process.env.EMAIL_FIELD],
           role: { '!=': [sails.config.globals.ROLE_DOCTOR, sails.config.globals.ROLE_ADMIN] }
         });
 
-        if (conflictingUser) {
+        if (conflictingUsers && conflictingUsers.length > 0) {
           return cb(new Error('A user with this email already exists with a different role'));
         }
 
