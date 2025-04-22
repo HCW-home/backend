6f1223e34536865cff0b412ce50ee6ad24a7e11e
Gor Grigoryan <gorgrigoryan1997@gmail.com>
2025-04-22 19:03:12 +0200

    Recreate TTL index to ensure correct expireAfterSeconds value

--- api/utils/helpers.js
+++ api/utils/helpers.js
@@ -110,10 +110,26 @@ function sanitizeMetadata(obj) {
   }
 }
 
+async function recreateTTLIndex(collection, field, expireAfterSeconds) {
+  const indexName = `${field}_1`;
+  try {
+    await collection.dropIndex(indexName);
+    sails.config.customLogger.log('info', `Dropped existing TTL index: ${indexName}`, null, 'server-action');
+  } catch (err) {
+    if (err?.codeName !== 'IndexNotFound') {
+      throw err;
+    }
+  }
+  await collection.createIndex({ [field]: 1 }, { expireAfterSeconds });
+  sails.config.customLogger.log('info', `Created TTL index on '${field}' with expiry: ${expireAfterSeconds}s`, null, 'server-action');
+}
+
+
 module.exports = {
   parseTime,
   escapeHtml,
   sanitizeMetadata,
+  recreateTTLIndex,
   importFileIfExists,
   createParamsFromJson,
 };
